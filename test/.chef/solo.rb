3572985272000b9a92ed2d0243bc51449fe8a3bc
Sergiu <asd@asd-PC>
2014-10-12 13:36:45 +0300

    Setting berkshellf and cookbook path

--- test/.chef/solo.rb
+++ test/.chef/solo.rb
@@ -32,6 +32,7 @@ log_location       STDOUT
 # this as a search location, in Array order. It should be a subdirectory of
 # file_cache_path, above.
 
+cookbook_path "/tmp/cookbooks"
 role_path "/home/travis/build/sergiuionescu/drush/roles"
 
 # recipe_url specifies a remote URL to retrieve a tarball of cookbooks.
